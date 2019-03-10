c4c96f2c19ab6f173e114be785250e8357d58cc4
Matías Olivera <matiasolivera@outlook.com>
2019-03-10 09:53:48 -0300

    Create the dropdown colors file

--- src/colors/editor/index.ts
+++ src/colors/editor/index.ts
@@ -1,9 +1,11 @@
 import baseColors from './base.colors';
 import textColors from './text.colors';
 import buttonColors from './button.colors';
+import dropdownColors from './dropdown.colors';
 
 export default {
   baseColors,
   textColors,
-  buttonColors
+  buttonColors,
+  dropdownColors
 };

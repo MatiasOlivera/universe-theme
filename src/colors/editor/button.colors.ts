91dcd01f67f558f95e7889c6d4c5965fa26daded
Matías Olivera <matiasolivera@outlook.com>
2019-03-10 09:50:45 -0300

    Create the button colors file

---
+++ src/colors/editor/button.colors.ts
@@ -0,0 +1,11 @@
+import { EditorColors } from '../../types/colors-types';
+
+const buttonColors: EditorColors = {
+  button: {
+    background: '#f4da55',
+    foreground: '#3d2e12',
+    hoverBackground: '#f7e87a'
+  }
+};
+
+export default buttonColors;
--- src/colors/editor/index.ts
+++ src/colors/editor/index.ts
@@ -1,7 +1,9 @@
 import baseColors from './base.colors';
 import textColors from './text.colors';
+import buttonColors from './button.colors';
 
 export default {
   baseColors,
-  textColors
+  textColors,
+  buttonColors
 };

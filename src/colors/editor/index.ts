5e1a3a69a1af5e115dde6e519bf8e1d1d654c3bf
Matías Olivera <matiasolivera@outlook.com>
2019-03-10 09:59:38 -0300

    Create the input colors file

--- src/colors/editor/index.ts
+++ src/colors/editor/index.ts
@@ -2,10 +2,12 @@ import baseColors from './base.colors';
 import textColors from './text.colors';
 import buttonColors from './button.colors';
 import dropdownColors from './dropdown.colors';
+import inputColors from './input.colors';
 
 export default {
   baseColors,
   textColors,
   buttonColors,
-  dropdownColors
+  dropdownColors,
+  inputColors
 };
---
+++ src/colors/editor/input.colors.ts
@@ -0,0 +1,26 @@
+import { EditorColors } from '../../types/colors-types';
+
+const inputColors: EditorColors = {
+  input: {
+    background: '#233345',
+    border: '#293c52',
+    foreground: '#f6f8fa',
+    placeholderForeground: '#96a2af'
+  },
+  inputOption: {
+    activeBorder: '#ad82f2'
+  },
+  inputValidation: {
+    errorBackground: '#4e1717',
+    errorBorder: '#701617',
+    errorForeground: '#ffeeee',
+    infoBackground: '#213147',
+    infoBorder: '#243953',
+    infoForeground: '#dceff9',
+    warningBackground: '#3d2e12',
+    warningBorder: '#4c3912',
+    warningForeground: '#f9f7d5'
+  }
+};
+
+export default inputColors;

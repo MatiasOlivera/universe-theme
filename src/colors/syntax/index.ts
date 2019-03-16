9e69c103114f8c70da3d85c3c746699503799156
Matías Olivera <matiasolivera@outlook.com>
2019-03-16 18:53:47 -0300

    Add CSS support

--- src/colors/syntax/index.ts
+++ src/colors/syntax/index.ts
@@ -1,9 +1,11 @@
 import commonColors from './common.colors';
+import cssColors from './css.colors';
 import htmlColors from './html.colors';
 import jsonColors from './json.colors';
 
 export default {
   commonColors,
+  cssColors,
   htmlColors,
   jsonColors
 };

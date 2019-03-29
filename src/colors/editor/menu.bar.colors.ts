f93a8b42d0f9701cea0cea1f2cb593da119d3ca8
Matías Olivera <matiasolivera@outlook.com>
2019-03-29 17:33:19 -0300

    fix: place the menus in front of the background

--- src/colors/editor/menu.bar.colors.ts
+++ src/colors/editor/menu.bar.colors.ts
@@ -1,20 +1,20 @@
 import { EditorColors } from '../../types/colors-types';
-import { background_500, background_600, background_700 } from '../palette/background.colors';
-import { gray_100 } from '../palette/gray.colors';
+import { background_400, background_500 } from '../palette/background.colors';
+import { gray_100, gray_700 } from '../palette/gray.colors';
 import { yellow_400, yellow_900 } from '../palette/yellow.colors';
 
 const menuBarColors: EditorColors = {
   menu: {
-    background: background_500,
+    background: background_400,
     foreground: gray_100,
     selectionBackground: yellow_400,
     selectionBorder: yellow_400,
     selectionForeground: yellow_900,
-    separatorBackground: background_700
+    separatorBackground: gray_700
   },
   menubar: {
-    selectionBackground: background_500,
-    selectionBorder: background_600,
+    selectionBackground: background_400,
+    selectionBorder: background_500,
     selectionForeground: gray_100
   }
 };

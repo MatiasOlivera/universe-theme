7b29ae5b1e64aa942338c52e71b61cb9e1dd3334
Matías Olivera <matiasolivera@outlook.com>
2019-04-01 04:09:52 -0300

    feat: add opacity values

---
+++ src/colors/palette/opacity.ts
@@ -0,0 +1,13 @@
+// + transparent
+export const opacity_0: string = '00';
+export const opacity_10: string = '1A';
+export const opacity_20: string = '33';
+export const opacity_30: string = '4D';
+export const opacity_40: string = '66';
+export const opacity_50: string = '80';
+export const opacity_60: string = '99';
+export const opacity_70: string = 'B3';
+export const opacity_80: string = 'CC';
+export const opacity_90: string = 'E6';
+export const opacity_100: string = 'FF';
+// + opaque

0dc3229863653d4e7fa4a4459eb527915b97e272
Matías Olivera <matiasolivera@outlook.com>
2019-04-01 04:18:08 -0300

    feat: add transparency to some colors

--- src/colors/editor/merge.conflict.colors.ts
+++ src/colors/editor/merge.conflict.colors.ts
@@ -3,8 +3,12 @@ import { background_700 } from '../palette/background.colors';
 import { blue_800, blue_900 } from '../palette/blue.colors';
 import { gray_200, gray_800, gray_900 } from '../palette/gray.colors';
 import { green_200 } from '../palette/green.colors';
+import { opacity_50 } from '../palette/opacity';
 import { purple_200 } from '../palette/purple.colors';
 import { teal_800, teal_900 } from '../palette/teal.colors';
+
+const headerOpacity: string = opacity_50;
+const contentOpacity: string = opacity_50;
 
 const mergeConflictColors: EditorColors = {
   editorOverviewRuler: {
@@ -14,12 +18,12 @@ const mergeConflictColors: EditorColors = {
   },
   merge: {
     border: background_700,
-    commonContentBackground: gray_900,
-    commonHeaderBackground: gray_800,
-    currentContentBackground: teal_800,
-    currentHeaderBackground: teal_900,
-    incomingContentBackground: blue_800,
-    incomingHeaderBackground: blue_900
+    commonContentBackground: `${gray_900}${contentOpacity}`,
+    commonHeaderBackground: `${gray_800}${headerOpacity}`,
+    currentContentBackground: `${teal_800}${contentOpacity}`,
+    currentHeaderBackground: `${teal_900}${headerOpacity}`,
+    incomingContentBackground: `${blue_800}${contentOpacity}`,
+    incomingHeaderBackground: `${blue_900}${headerOpacity}`
   }
 };
 

ad93fbad5613109c530f6e64d42ac235d1b5391b
Matías Olivera <matiasolivera@outlook.com>
2019-04-19 18:50:43 -0300

    feat: update base colors

--- src/colors/editor/base.colors.ts
+++ src/colors/editor/base.colors.ts
@@ -1,28 +1,29 @@
 import { UIColors } from '../../types/colors-types';
+import { opacity_50 } from '../opacity';
 
 const baseColors: UIColors = (palette) => {
-  const { red, blueGray, yellow, deepPurple } = palette;
+  const { primary, blueGray, red } = palette;
 
   return {
     // Overall foreground color.
-    foreground: blueGray[1],
+    foreground: blueGray[0],
 
     // Foreground color for description text providing additional information,
     // for example for a label.
-    descriptionForeground: blueGray[3],
+    descriptionForeground: blueGray[4],
 
     // Overall foreground color for error messages
-    errorForeground: red[1],
+    errorForeground: red[2],
 
     // Overall border color for focused elements. This color is only used if
     // not overridden by a component.
-    focusBorder: yellow[3],
+    focusBorder: primary[3],
 
     selection: {
       // Background color of text selections in the workbench (for input fields
       // or text areas, does not apply to selections within the editor and
       // the terminal).
-      background: deepPurple[5]
+      background: `${primary[3]}${opacity_50}`
     }
   };
 };

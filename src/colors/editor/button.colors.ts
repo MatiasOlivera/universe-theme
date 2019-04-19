bfbd81e3f5756fcff806ef20295980b293ad3e68
Matías Olivera <matiasolivera@outlook.com>
2019-04-19 19:43:44 -0300

    feat: update button colors

--- src/colors/editor/button.colors.ts
+++ src/colors/editor/button.colors.ts
@@ -3,18 +3,18 @@ import { UIColors } from '../../types/colors-types';
 // A set of colors for button widgets such as Open Folder button in the
 // Explorer of a new window.
 const buttonColors: UIColors = (palette) => {
-  const { yellow, gray } = palette;
+  const { secondary, gray } = palette;
 
   return {
     button: {
       // Button background color.
-      background: yellow[3],
+      background: secondary[3],
 
       // Button foreground color.
       foreground: gray[9],
 
       // Button background color when hovering.
-      hoverBackground: yellow[2]
+      hoverBackground: secondary[2]
     }
   };
 };

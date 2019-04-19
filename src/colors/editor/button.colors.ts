eb19aae4cc2ad91955b3f8720bef0d3cb8a33fe9
Matías Olivera <matiasolivera@outlook.com>
2019-04-19 19:34:40 -0300

    docs: add descriptions to button colors

--- src/colors/editor/button.colors.ts
+++ src/colors/editor/button.colors.ts
@@ -1,12 +1,19 @@
 import { UIColors } from '../../types/colors-types';
 
+// A set of colors for button widgets such as Open Folder button in the
+// Explorer of a new window.
 const buttonColors: UIColors = (palette) => {
   const { yellow, gray } = palette;
 
   return {
     button: {
+      // Button background color.
       background: yellow[3],
+
+      // Button foreground color.
       foreground: gray[9],
+
+      // Button background color when hovering.
       hoverBackground: yellow[2]
     }
   };

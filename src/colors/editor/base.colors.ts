e1b8804788e5153353c9eba4375145e77a8c05aa
Matías Olivera <matiasolivera@outlook.com>
2019-04-19 18:35:57 -0300

    docs: add descriptions to base colors

--- src/colors/editor/base.colors.ts
+++ src/colors/editor/base.colors.ts
@@ -4,11 +4,24 @@ const baseColors: UIColors = (palette) => {
   const { red, blueGray, yellow, deepPurple } = palette;
 
   return {
-    descriptionForeground: blueGray[3],
-    errorForeground: red[1],
-    focusBorder: yellow[3],
+    // Overall foreground color.
     foreground: blueGray[1],
+
+    // Foreground color for description text providing additional information,
+    // for example for a label.
+    descriptionForeground: blueGray[3],
+
+    // Overall foreground color for error messages
+    errorForeground: red[1],
+
+    // Overall border color for focused elements. This color is only used if
+    // not overridden by a component.
+    focusBorder: yellow[3],
+
     selection: {
+      // Background color of text selections in the workbench (for input fields
+      // or text areas, does not apply to selections within the editor and
+      // the terminal).
       background: deepPurple[5]
     }
   };

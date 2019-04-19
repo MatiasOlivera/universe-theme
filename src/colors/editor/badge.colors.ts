a77990169f458b6a6253eab0a10014d80f5537d1
Matías Olivera <matiasolivera@outlook.com>
2019-04-19 20:32:30 -0300

    docs: add descriptions to badge colors

--- src/colors/editor/badge.colors.ts
+++ src/colors/editor/badge.colors.ts
@@ -1,11 +1,15 @@
 import { UIColors } from '../../types/colors-types';
 
+// Badges are small information labels, for example, search results count.
 const badgeColors: UIColors = (palette) => {
   const { yellow, gray } = palette;
 
   return {
     badge: {
+      // Badge foreground color.
       foreground: gray[9],
+
+      // Badge background color.
       background: yellow[3]
     }
   };

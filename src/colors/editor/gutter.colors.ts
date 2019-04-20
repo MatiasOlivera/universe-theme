ed16d2504e83469b3be2a345e12d5210ff826cc8
Matías Olivera <matiasolivera@outlook.com>
2019-04-20 11:33:17 -0300

    docs: add descriptions to gutter colors

--- src/colors/editor/gutter.colors.ts
+++ src/colors/editor/gutter.colors.ts
@@ -1,15 +1,24 @@
 import { UIColors } from '../../types/colors-types';
 
+// The gutter contains the glyph margins and the line numbers
 const gutterColors: UIColors = (palette) => {
   const { green, background, blueGray, red, blue } = palette;
 
   return {
     editorGutter: {
-      addedBackground: green[1],
+      // Background color of the editor gutter.
       background: background[5],
-      commentRangeForeground: blueGray[1],
+
+      // Editor gutter background color for lines that are added.
+      addedBackground: green[1],
+
+      // Editor gutter background color for lines that are modified.
+      modifiedBackground: blue[1],
+
+      // Editor gutter background color for lines that are deleted.
       deletedBackground: red[1],
-      modifiedBackground: blue[1]
+
+      commentRangeForeground: blueGray[1]
     }
   };
 };

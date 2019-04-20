72eba033da78bb27d2927d4a26d248a921496177
Matías Olivera <matiasolivera@outlook.com>
2019-04-20 11:25:16 -0300

    docs: add descriptions to squiggles colors

--- src/colors/editor/squiggles.colors.ts
+++ src/colors/editor/squiggles.colors.ts
@@ -5,15 +5,19 @@ const squigglesColors: UIColors = (palette) => {
 
   return {
     editorError: {
+      // Foreground color of error squiggles in the editor.
       foreground: red[2]
     },
     editorHint: {
+      // Foreground color of hints in the editor.
       foreground: deepPurple[2]
     },
     editorInfo: {
+      // Foreground color of info squiggles in the editor.
       foreground: blue[2]
     },
     editorWarning: {
+      // Foreground color of warning squiggles in the editor.
       foreground: yellow[2]
     }
   };

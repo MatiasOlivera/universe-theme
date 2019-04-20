6c9e5caa2daf0493b2b50e3d8825d9eca6a795f0
Matías Olivera <matiasolivera@outlook.com>
2019-04-20 20:47:47 -0300

    feat: update merge confilict colors

--- src/colors/editor/merge.conflict.colors.ts
+++ src/colors/editor/merge.conflict.colors.ts
@@ -1,53 +1,53 @@
 import { UIColors } from '../../types/colors-types';
-import { opacity_50 } from '../opacity';
+import { opacity_0, opacity_10, opacity_20 } from '../opacity';
 
 // Merge conflict decorations are shown when the editor contains special diff
 // ranges.
 const mergeConflictColors: UIColors = (palette) => {
-  const { gray, green, deepPurple, background, cyan, blue } = palette;
+  const { lightBlue, lightGreen, background } = palette;
 
-  const headerOpacity: string = opacity_50;
-  const contentOpacity: string = opacity_50;
+  const headerOpacity: string = opacity_20;
+  const contentOpacity: string = opacity_10;
 
   return {
     editorOverviewRuler: {
       // Common ancestor overview ruler foreground for inline merge conflicts.
-      commonContentForeground: gray[1],
+      commonContentForeground: background[0],
 
       // Current overview ruler foreground for inline merge conflicts.
-      currentContentForeground: green[1],
+      currentContentForeground: lightBlue[3],
 
       // Incoming overview ruler foreground for inline merge conflicts.
-      incomingContentForeground: deepPurple[1]
+      incomingContentForeground: lightGreen[3]
     },
 
     merge: {
       // Current header background in inline merge conflicts. The color must not
       // be opaque so as not to hide underlying decorations.
-      currentHeaderBackground: `${cyan[9]}${headerOpacity}`,
+      currentHeaderBackground: `${lightBlue[3]}${headerOpacity}`,
 
       // Current content background in inline merge conflicts. The color must
       // not be opaque so as not to hide underlying decorations.
-      currentContentBackground: `${cyan[7]}${contentOpacity}`,
+      currentContentBackground: `${lightBlue[3]}${contentOpacity}`,
 
       // Incoming header background in inline merge conflicts. The color must
       // not be opaque so as not to hide underlying decorations.
-      incomingHeaderBackground: `${blue[9]}${headerOpacity}`,
+      incomingHeaderBackground: `${lightGreen[3]}${headerOpacity}`,
 
       //  Incoming content background in inline merge conflicts. The color must
       // not be opaque so as not to hide underlying decorations.
-      incomingContentBackground: `${blue[7]}${contentOpacity}`,
+      incomingContentBackground: `${lightGreen[3]}${contentOpacity}`,
 
       // Common ancestor header background in inline merge-conflicts. The color
       // must not be opaque so as not to hide underlying decorations.
-      commonHeaderBackground: `${gray[7]}${headerOpacity}`,
+      commonHeaderBackground: `${background[1]}${headerOpacity}`,
 
       // Common ancestor content background in inline merge-conflicts. The color
       // must not be opaque so as not to hide underlying decorations.
-      commonContentBackground: `${gray[9]}${contentOpacity}`,
+      commonContentBackground: `${background[1]}${contentOpacity}`,
 
       // Border color on headers and the splitter in inline merge conflicts.
-      border: background[6]
+      border: `${background[0]}${opacity_0}`
     }
   };
 };

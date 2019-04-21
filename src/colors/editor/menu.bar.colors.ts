6ad1cfdb9fac78075614eb199c26e1aaa0554baf
Matías Olivera <matiasolivera@outlook.com>
2019-04-20 22:42:06 -0300

    docs: add descriptions to menu bar colors

--- src/colors/editor/menu.bar.colors.ts
+++ src/colors/editor/menu.bar.colors.ts
@@ -7,17 +7,34 @@ const menuBarColors: UIColors = (palette) => {
 
   return {
     menu: {
+      // Background color of menu items.
       background: dialogBackground,
+
+      // Foreground color of menu items.
       foreground: blueGray[0],
-      selectionBackground: yellow[3],
-      selectionBorder: yellow[3],
+
+      // Foreground color of the selected menu item in menus.
       selectionForeground: gray[9],
+
+      // Background color of the selected menu item in menus.
+      selectionBackground: yellow[3],
+
+      // Border color of the selected menu item in menus.
+      selectionBorder: yellow[3],
+
+      // Color of a separator menu item in menus.
       separatorBackground: blueGray[6]
     },
+
     menubar: {
+      // Foreground color of the selected menu item in the menubar.
+      selectionForeground: blueGray[0],
+
+      // Background color of the selected menu item in the menubar.
       selectionBackground: dialogBackground,
-      selectionBorder: dialogBackground,
-      selectionForeground: blueGray[0]
+
+      // Border color of the selected menu item in the menubar.
+      selectionBorder: dialogBackground
     }
   };
 };

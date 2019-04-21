8eb93b81e8c2338b3b8481a7fcd5654343d64a04
Matías Olivera <matiasolivera@outlook.com>
2019-04-20 22:33:54 -0300

    feat: update title bar colors

--- src/colors/editor/title.bar.colors.ts
+++ src/colors/editor/title.bar.colors.ts
@@ -2,20 +2,21 @@ import { UIColors } from '../../types/colors-types';
 
 const titleBarColors: UIColors = (palette) => {
   const { background, blueGray } = palette;
+  const backgroundColor: string = background[7];
 
   return {
     titleBar: {
       // Title Bar background when the window is active.
-      activeBackground: background[7],
+      activeBackground: backgroundColor,
 
       // Title Bar foreground when the window is active.
-      activeForeground: blueGray[2],
+      activeForeground: blueGray[5],
 
       // Title Bar background when the window is inactive.
-      inactiveBackground: background[7],
+      inactiveBackground: backgroundColor,
 
       // Title Bar foreground when the window is inactive.
-      inactiveForeground: blueGray[3],
+      inactiveForeground: blueGray[6],
 
       // Title bar border color.
       border: background[8]

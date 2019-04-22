dfd77349b9fa788c3e00ab23f9ff7f72a38341bd
Matías Olivera <matiasolivera@outlook.com>
2019-04-21 22:22:26 -0300

    feat: update breadcumb colors

--- src/colors/editor/breadcrumb.colors.ts
+++ src/colors/editor/breadcrumb.colors.ts
@@ -1,8 +1,10 @@
 import { UIColors } from '../../types/colors-types';
+import { dialogTokens } from './dialog.colors';
 
 // The theme colors for breadcrumbs navigation
 const breadcrumbColors: UIColors = (palette) => {
   const { blueGray, background } = palette;
+  const { dialogBackground } = dialogTokens(palette);
 
   return {
     breadcrumb: {
@@ -10,10 +12,10 @@ const breadcrumbColors: UIColors = (palette) => {
       background: background[5],
 
       // Color of breadcrumb items.
-      foreground: blueGray[2],
+      foreground: blueGray[3],
 
       // Color of focused breadcrumb items.
-      focusForeground: blueGray[1],
+      focusForeground: blueGray[2],
 
       // Color of selected breadcrumb items.
       activeSelectionForeground: blueGray[1]
@@ -21,7 +23,7 @@ const breadcrumbColors: UIColors = (palette) => {
 
     breadcrumbPicker: {
       // Background color of breadcrumb item picker.
-      background: background[4]
+      background: dialogBackground
     }
   };
 };

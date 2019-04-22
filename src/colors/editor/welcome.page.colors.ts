1d982b6f3b02ef6ee107be6cb4156d2345976f53
Matías Olivera <matiasolivera@outlook.com>
2019-04-21 21:08:53 -0300

    docs: add descriptions to welcome page colors

--- src/colors/editor/welcome.page.colors.ts
+++ src/colors/editor/welcome.page.colors.ts
@@ -5,11 +5,18 @@ const welcomePageColors: UIColors = (palette) => {
 
   return {
     walkThrough: {
+      // Background color for the embedded editors on the Interactive Playground.
       embeddedEditorBackground: background[5]
     },
+
     welcomePage: {
+      // Background color for the Welcome page.
       background: background[5],
+
+      // Background color for the buttons on the Welcome page.
       buttonBackground: background[6],
+
+      // Hover background color for the buttons on the Welcome page.
       buttonHoverBackground: deepPurple[7]
     }
   };

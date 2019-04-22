4c2b353cc886cdf9486e8cc29d574d8d05f685a9
Matías Olivera <matiasolivera@outlook.com>
2019-04-21 21:18:22 -0300

    docs: add descriptions to git colors

--- src/colors/editor/git.colors.ts
+++ src/colors/editor/git.colors.ts
@@ -1,17 +1,31 @@
 import { UIColors } from '../../types/colors-types';
 
+// Used for file labels and the SCM viewlet.
 const gitColors: UIColors = (palette) => {
   const { green, yellow, red, blueGray, blue, deepPurple } = palette;
 
   return {
     gitDecoration: {
+      // Color for added Git resources.
       addedResourceForeground: green[1],
-      conflictingResourceForeground: yellow[1],
-      deletedResourceForeground: red[1],
-      ignoredResourceForeground: blueGray[1],
+
+      // Color for modified Git resources.
       modifiedResourceForeground: blue[1],
-      submoduleResourceForeground: blueGray[0],
-      untrackedResourceForeground: deepPurple[1]
+
+      // Color for deleted Git resources.
+      deletedResourceForeground: red[1],
+
+      // Color for untracked Git resources.
+      untrackedResourceForeground: deepPurple[1],
+
+      // Color for ignored Git resources.
+      ignoredResourceForeground: blueGray[1],
+
+      // Color for conflicting Git resources.
+      conflictingResourceForeground: yellow[1],
+
+      //  Color for submodule resources.
+      submoduleResourceForeground: blueGray[0]
     }
   };
 };

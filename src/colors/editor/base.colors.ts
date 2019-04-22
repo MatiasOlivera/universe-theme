640c80420567f1c3964c129bae332fa55db140c8
Matías Olivera <matiasolivera@outlook.com>
2019-04-21 22:42:17 -0300

    fix: use more discrete colors

--- src/colors/editor/base.colors.ts
+++ src/colors/editor/base.colors.ts
@@ -1,29 +1,30 @@
 import { UIColors } from '../../types/colors-types';
-import { opacity_50 } from '../opacity';
+import { selectionTokens } from './editor.colors';
 
 const baseColors: UIColors = (palette) => {
-  const { primary, blueGray, red } = palette;
+  const { blueGray, red, background } = palette;
+  const { selectionBackground } = selectionTokens(palette);
 
   return {
     // Overall foreground color.
-    foreground: blueGray[0],
+    foreground: blueGray[2],
 
     // Foreground color for description text providing additional information,
     // for example for a label.
-    descriptionForeground: blueGray[4],
+    descriptionForeground: blueGray[5],
 
     // Overall foreground color for error messages
     errorForeground: red[2],
 
     // Overall border color for focused elements. This color is only used if
     // not overridden by a component.
-    focusBorder: primary[3],
+    focusBorder: background[0],
 
     selection: {
       // Background color of text selections in the workbench (for input fields
       // or text areas, does not apply to selections within the editor and
       // the terminal).
-      background: `${primary[3]}${opacity_50}`
+      background: selectionBackground
     }
   };
 };

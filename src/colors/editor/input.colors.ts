325b80e0a3fc0ddd4046e8ec1ecbc46bc29601fa
Matías Olivera <matiasolivera@outlook.com>
2019-05-08 18:49:37 -0300

    fix: use primary color in border of activated options in input fields
    It has much more contrast

--- src/colors/editor/input.colors.ts
+++ src/colors/editor/input.colors.ts
@@ -19,7 +19,7 @@ export const inputTokens = (palette: ColorPalettes): InputTokens => {
 // Colors for input controls such as in the Search view or the Find/Replace
 // dialog.
 const inputColors: UIColors = (palette) => {
-  const { background, neutral, red, blue, yellow } = palette;
+  const { background, neutral, red, blue, yellow, primary } = palette;
   const { inputBackground, inputBorder, inputForeground } = inputTokens(
     palette
   );
@@ -41,7 +41,7 @@ const inputColors: UIColors = (palette) => {
 
     inputOption: {
       // Border color of activated options in input fields.
-      activeBorder: background[0]
+      activeBorder: primary[4]
     },
 
     inputValidation: {

006781ae42479b8916222a8490afe86afe7a6480
Matías Olivera <matiasolivera@outlook.com>
2019-05-08 18:23:12 -0300

    fix: use transparent background in diff editor
    It makes reading the code easier

--- src/colors/editor/diff.colors.ts
+++ src/colors/editor/diff.colors.ts
@@ -1,5 +1,5 @@
 import { UIColors } from '../../types/colors-types';
-import { opacity_20 } from '../opacity';
+import { opacity_10 } from '../opacity';
 
 // For coloring inserted and removed text, use either a background or a border
 // color but not both.
@@ -10,11 +10,11 @@ const diffColors: UIColors = (palette) => {
     diffEditor: {
       // Background color for text that got inserted. The color must not be
       // opaque so as not to hide underlying decorations.
-      insertedTextBackground: `${green[4]}${opacity_20}`,
+      insertedTextBackground: `${green[4]}${opacity_10}`,
 
       // Background color for text that got removed. The color must not be
       // opaque so as not to hide underlying decorations.
-      removedTextBackground: `${red[4]}${opacity_20}`,
+      removedTextBackground: `${red[4]}${opacity_10}`,
 
       // Border color between the two text editors.
       border: background[6]
